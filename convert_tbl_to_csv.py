5077d2f516394dddd78824e61da8c103494c21fc
Lenka <lenkahuang07@gmail.com>
2024-04-05 15:35:51 +0200

    defined file class

--- convert_tbl_to_csv.py
+++ convert_tbl_to_csv.py
@@ -1,8 +1,43 @@
 import csv
+import os
 
 COLUMN_LABELS = ["CENTRALWAVELNG", "BANDWIDTH", "PL_TRANDEP", "PL_TRANDEPERR1", "PL_TRANDEPERR2", "PL_TRANDEPLIM", "PL_TRANDEP_AUTHORS", "PL_TRANDEP_URL", "PL_RATROR", "PL_RATRORERR1", "PL_RATRORERR2", "PL_RATRORLIM", "PL_RATROR_AUTHORS", "PL_RATROR_URL", "PL_RADJ", "PL_RADJERR1", "PL_RADJERR2", "PL_RADJLIM", "PL_RADJ_AUTHORS", "PL_RADJ_URL", "PL_TRANMID", "PL_TRANMIDERR1", "PL_TRANMIDERR2", "PL_TRANMIDLIM", "ST_RAD", "ST_RADERR1", "ST_RADERR2", "ST_RAD_AUTHORS", "ST_RAD_URL"]
 
+class File:
+    def __init__(self, path, start_index, end_index, info):
+        """
+        include path of the raw data file
 
+        start index (including) -> end index (excluding) in the processed csv file
+        """
+        self.path = path
+        self.loc_in_csv = (start_index, end_index)
+        self.specs = dict()
+
+        self.add_info(info)
+    
+    def __str__(self):
+
+        return f"""FILE DETAILS FOR: {self.path} 
+            Planet Name: {self.specs["planet_name"]}
+            Spectrum Type: {self.specs["spectrum_type"]}
+            Instrument: {self.specs["instrument"]}
+            Facility: {self.specs["facility"]}
+            Note: {self.specs["note"]}
+            Reference: {self.specs["reference"]}
+
+            DATA STORAGE DETAILS:
+            In clean_csv > all_data.csv
+            Index {self.loc_in_csv[0]} to and not including index {self.loc_in_csv[1]}
+            """
+
+    
+    def add_info(self, info):
+        info_labels = ["planet_name", "spectrum_type", "instrument", "facility", "note", "reference"]
+        
+        for i, label in enumerate(info_labels):
+            self.specs[label] = info[i]
+    
 def reformat_data(file_name):
     """
     Reads the raw tbl files and processes the raw values data
@@ -30,17 +65,30 @@ def reformat_data(file_name):
 
             else:
                 items = line.split(" ")
-                items = [x.strip() for x in items if x]
+                items = [x.strip() for x in items if len(x.strip())]
 
                 DATA.append(items)
 
-    print(len(COLUMN_LABELS))
-    print(len(DATA[0]))
+    # for i, item in enumerate(COLUMN_LABELS):
+    #     print(item)
+    #     print(DATA[0][i])
+    #     print('\n')
+
+    # print(len(COLUMN_LABELS))
+    # print(len(DATA[0]))
+    # print(DATA)
+
     return INFO, DATA
 
+def disassemble_info(info):
+    raise NotImplementedError
+
+def write_data_to_csv(data):
+    raise NotImplementedError
 
 def main():
-    reformat_data('./WASP-39b Data/WASP_39_b_3.11466_4132_1.tbl')
+    # reformat_data('./WASP-39b Data/WASP_39_b_3.11466_4132_1.tbl')
+    reformat_data('./WASP-39b Data/WASP_39_b_3.11466_3491_1.tbl')
 
 if __name__ == "__main__":
     main()
