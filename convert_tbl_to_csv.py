8f0e73925ac0d456ade1a4f3633881e0357e7065
Lenka <lenkahuang07@gmail.com>
2024-04-05 15:57:45 +0200

    specs stripping function

--- convert_tbl_to_csv.py
+++ convert_tbl_to_csv.py
@@ -3,6 +3,8 @@ import os
 
 COLUMN_LABELS = ["CENTRALWAVELNG", "BANDWIDTH", "PL_TRANDEP", "PL_TRANDEPERR1", "PL_TRANDEPERR2", "PL_TRANDEPLIM", "PL_TRANDEP_AUTHORS", "PL_TRANDEP_URL", "PL_RATROR", "PL_RATRORERR1", "PL_RATRORERR2", "PL_RATRORLIM", "PL_RATROR_AUTHORS", "PL_RATROR_URL", "PL_RADJ", "PL_RADJERR1", "PL_RADJERR2", "PL_RADJLIM", "PL_RADJ_AUTHORS", "PL_RADJ_URL", "PL_TRANMID", "PL_TRANMIDERR1", "PL_TRANMIDERR2", "PL_TRANMIDLIM", "ST_RAD", "ST_RADERR1", "ST_RADERR2", "ST_RAD_AUTHORS", "ST_RAD_URL"]
 
+ALL_FILES = list()
+
 class File:
     def __init__(self, path, start_index, end_index, info):
         """
@@ -42,11 +44,19 @@ def reformat_data(file_name):
     """
     Reads the raw tbl files and processes the raw values data
     :param file_name:
-    :return 2D list of data based on the global column labels:
+    :return 1D list of the specs and 2D list of data based on the global column labels:
 
     FOR ALL DATA FILES:
     - make sure all the names-dates are connected via DASH!
     e.g. "Wakeford et al. 2018" -> "Wakeford-et-al.-2018"
+
+    INPUT RAW DATA FILE FORMAT
+    \\NAME FIELD = SPECS VALUE
+    \\ NAME FIELD = SPECS VALUE
+    \\
+    \\
+    |COLUMN TITLES | COLUMN TITLES | ... |
+              data |         data  | ... |
     """
 
     INFO = list()
@@ -57,8 +67,8 @@ def reformat_data(file_name):
         # print(len(all_lines))
 
         for line in all_lines:
-            if line[0] == "\\":
-                INFO.append(line.strip()[1:])
+            if line[0] == "\\" and line[1:].strip():
+                INFO.append(line.strip()[1:].split(" = ")[-1])
 
             elif line[0] == "|":
                 pass
@@ -69,26 +79,16 @@ def reformat_data(file_name):
 
                 DATA.append(items)
 
-    # for i, item in enumerate(COLUMN_LABELS):
-    #     print(item)
-    #     print(DATA[0][i])
-    #     print('\n')
-
-    # print(len(COLUMN_LABELS))
-    # print(len(DATA[0]))
-    # print(DATA)
-
     return INFO, DATA
 
-def disassemble_info(info):
-    raise NotImplementedError
 
 def write_data_to_csv(data):
     raise NotImplementedError
 
 def main():
     # reformat_data('./WASP-39b Data/WASP_39_b_3.11466_4132_1.tbl')
-    reformat_data('./WASP-39b Data/WASP_39_b_3.11466_3491_1.tbl')
+    i, d = reformat_data('./WASP-39b Data/WASP_39_b_3.11466_3491_1.tbl')
+    print(i)
 
 if __name__ == "__main__":
     main()
