d1943c8878c1a1f3404b80ddf1923dea2f158a52
Lenka <lenkahuang07@gmail.com>
2024-04-09 19:01:05 +0200

    fetching database + sorting

--- fetching_atomic_spectra.py
+++ fetching_atomic_spectra.py
@@ -1,2 +1,19 @@
 from astroquery.nist import Nist
+import astropy.units as u
 
+def fetchData(lowerbound, upperbound, element):
+    """
+    Enter the lowerbound (in micrometers) and upperbound in micrometers
+    Returns a NIST table
+    """
+
+    # bounds 
+    table = Nist.query(lowerbound*1000 *u.nm, upperbound*1000 * u.nm, linename=element, wavelength_type="vacuum")
+
+    return table
+
+def main():
+    fetchData(400, 410, "H")
+
+if __name__ == "__main__":
+    main()
--- graph_data.py
+++ graph_data.py
@@ -1,4 +1,9 @@
 import matplotlib.pyplot as plt
+from astroquery.nist import Nist
+import astropy.units as u
+
+# MATPLOTLIB SUPPORTED COLORS
+COLORS = ['g', 'c', 'm', 'y', 'r']
 
 def sortData(min_wl=0, max_wl=10, remove_odd_points=True):
     """
@@ -48,22 +53,81 @@ def sortData(min_wl=0, max_wl=10, remove_odd_points=True):
                 
     return x, y, x_errorbars, y_errorbars
 
-def plot(x, y, x_err=[], y_err=[], title=''):
+def plot(x_data, y_data, x_err=[], y_err=[], title=''):
     """
     plots the data given
     """
+    
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.errorbar(x_data, y_data, xerr=x_err, yerr=y_err, fmt='.', ls='none')
 
-    plt.errorbar(x, y, xerr=x_err, yerr=y_err, fmt='.')
-
-    plt.xlabel("Wavelength (micrometers)", loc='center')
-    plt.ylabel("Transit Depth (%)", loc='center')
-    plt.title(title)
+    ax.set_xlabel("Wavelength (micrometers)", loc='center')
+    ax.set_ylabel("Transit Depth (%)", loc='center')
+    ax.set_title(title)
 
     plt.show()
 
+def fetchData(lowerbound, upperbound, element):
+    """
+    Enter the lowerbound (in nm) and upperbound in nm
+    Returns a list of x values in nm
+    """
+
+    print("FETCHING MOLECULAR LINES DATA...")
+
+    x = list()
+
+    # bounds 
+    table = Nist.query(lowerbound *u.nm, upperbound * u.nm, linename=element, wavelength_type="vacuum")
+    
+    # USING ONLY OBSERVED VALUES
+    for i in range(len(table)):
+        
+        # prevent if there are no observed values
+        if table[i][0] != '--':
+            x.append(round(float(table[i][0]), 3))
+
+    print(x)
+    return x
+
 def main():
-    x_vals, y_vals, xerr_bars, yerr_bars = sortData()
-    plot(x_vals, y_vals, xerr_bars, yerr_bars, "Transmission Spectrum of WASP-39b")
+
+    # TRANSMISSION SPECTRA DATA ANALYSIS -----------------------
+    # SETS THE GRAPHING BOUNDARY in micrometers
+    min_val, max_val = 0.4, 1
+
+    # sort data
+    x_vals, y_vals, xerr_bars, yerr_bars = sortData(min_val, max_val)
+
+    # Plots the normal data
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.errorbar(x_vals, y_vals, xerr=xerr_bars, yerr=yerr_bars, fmt='.', ls='none')
+
+    # MOLECULAR ANALYSIS -------------------------
+    # SETS THE TARGET MOLECULES in list for query 
+
+    target_molecules = []
+    # target_molecules = ["H"]
+    
+    # Fetch atomic data + graph
+    for i, molec in enumerate(target_molecules):
+
+        print(f"CONDUCTING MOLECULAR ANALYSIS for {molec}")
+
+        x_vals = fetchData(min_val*1000, max_val*1000, molec)
+        
+        for j in range(len(x_vals)):
+            ax.axvspan(x_vals[j]/1000, (x_vals[j]+0.001)/1000, alpha=0.5, color=COLORS[i])
+
+        
+    # Paper-proof the graph
+    ax.set_xlabel("Wavelength (micrometers)", loc='center')
+    ax.set_ylabel("Transit Depth (%)", loc='center')
+    ax.set_title(f"Transmission Spectrum of WASP-39b from {min_val} to {max_val} micrometers")
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
